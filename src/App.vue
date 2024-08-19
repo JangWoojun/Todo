fb44aa9491301a4bdcc88c3d367d80f704a251a4
JangWoojun <woojun0107@naver.com>
2024-08-19 19:34:36 +0900

    feat: 컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <ToDoList/>
+  <h1>Vue.js To-Do App</h1>
+  <add-todo @add-todo="addTodo" />
+  <to-do-list :todos="todos" @remove-todo="removeTodo" @save-todos="saveTodos" />
 </template>
 
 <script>
+  import AddTodo from './components/AddTodo.vue';
+  import ToDoList from './components/ToDoList.vue';
 
-import ToDoList from './components/ToDoList.vue';
-
-export default {
-  name: 'App',
-  components: {
-    ToDoList
+  export default {
+    name: 'App',
+    components: {
+      AddTodo,
+      ToDoList
+    },
+    data() {
+      return {
+        todos: []
+      };
+    },
+    mounted() {
+      const savedTodos = localStorage.getItem('todos');
+      if (savedTodos) {
+        this.todos = JSON.parse(savedTodos);
+      }
+    },
+    methods: {
+      addTodo(newTodo) {
+        this.todos.push({ text: newTodo, completed: false });
+        this.saveTodos();
+      },
+      removeTodo(index) {
+        this.todos.splice(index, 1);
+        this.saveTodos();
+      },
+      saveTodos() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,56 +1,36 @@
 <template>
-    <div>
-      <h2>To-Do List</h2>
-      <input v-model="newTodo" placeholder="Add a new to-do" @keyup.enter="addTodo" />
-      <button @click="addTodo">Add</button>
-  
-      <ul>
+    <ul>
         <li v-for="(todo, index) in todos" :key="index">
-          <input type="checkbox" v-model="todo.completed" @change="saveTodos" />
-          <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
-          <button @click="removeTodo(index)">Delete</button>
+            <input type="checkbox" v-model="todo.completed" @change="saveTodos" />
+            <span :class="{ completed: todo.completed }">{{ todo.text }}</span>
+            <button @click="removeTodo(index)">Delete</button>
         </li>
-      </ul>
-    </div>
-  </template>
+    </ul>
+</template>
   
-  <script>
+<script>
   export default {
     name: 'ToDoList',
-    data() {
-      return {
-        newTodo: '',
-        todos: []
-      };
-    },
-    mounted() {
-      const savedTodos = localStorage.getItem('todos');
-      if (savedTodos) {
-        this.todos = JSON.parse(savedTodos);
+    props: {
+      todos: {
+        type: Array,
+        required: true
       }
     },
     methods: {
-      addTodo() {
-        if (this.newTodo.trim()) {
-          this.todos.push({ text: this.newTodo, completed: false });
-          this.newTodo = '';
-          this.saveTodos();
-        }
-      },
       removeTodo(index) {
-        this.todos.splice(index, 1);
-        this.saveTodos(); 
+        this.$emit('remove-todo', index);
       },
       saveTodos() {
-        localStorage.setItem('todos', JSON.stringify(this.todos));
+        this.$emit('save-todos');
       }
     }
   };
-  </script>
+</script>
   
-  <style>
-  .completed {
+<style>
+    .completed {
     text-decoration: line-through;
     color: grey;
-  }
-  </style>
+    }
+</style>
