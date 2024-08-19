5ea49e8c3741cbf0a924c74039ea000eb129f3e2
JangWoojun <woojun0107@naver.com>
2024-08-19 20:11:33 +0900

    feat: Vuetify 적용

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,56 @@
 <template>
-  <h1>Vue.js To-Do App</h1>
-  <add-todo @add-todo="addTodo" />
-  <to-do-list :todos="todos" @remove-todo="removeTodo" @save-todos="saveTodos" />
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h1 class="text-center">Vue.js To-Do App</h1>
+          <AddTodo @add-todo="addTodo" />
+          <ToDoList :todos="todos" @remove-todo="removeTodo" @save-todos="saveTodos" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-  import AddTodo from './components/AddTodo.vue';
-  import ToDoList from './components/ToDoList.vue';
+import AddTodo from './components/AddTodo.vue';
+import ToDoList from './components/ToDoList.vue';
 
-  export default {
-    name: 'App',
-    components: {
-      AddTodo,
-      ToDoList
+export default {
+  name: 'App',
+  components: {
+    AddTodo,
+    ToDoList
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  mounted() {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      this.todos = JSON.parse(savedTodos);
+    }
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({ text: newTodo, completed: false });
+      this.saveTodos();
     },
-    data() {
-      return {
-        todos: []
-      };
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.saveTodos();
     },
-    mounted() {
-      const savedTodos = localStorage.getItem('todos');
-      if (savedTodos) {
-        this.todos = JSON.parse(savedTodos);
-      }
-    },
-    methods: {
-      addTodo(newTodo) {
-        this.todos.push({ text: newTodo, completed: false });
-        this.saveTodos();
-      },
-      removeTodo(index) {
-        this.todos.splice(index, 1);
-        this.saveTodos();
-      },
-      saveTodos() {
-        localStorage.setItem('todos', JSON.stringify(this.todos));
-      }
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
     }
   }
+};
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+.text-center {
+  text-align: center;
+}
 </style>
